aad38a3e726a3792b36ffc7516bd346529b69020
Konstantin Zaytcev <zaydisk@yandex.ru>
2024-01-04 11:54:01 +0300

    HW8 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,58 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+	"strings"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	if len(cmd) < 2 {
+		return 1
+	}
+
+	command := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+
+	newEnv := getNewEnv(env)
+	command.Env = make([]string, 0, len(newEnv))
+	for key, value := range newEnv {
+		command.Env = append(command.Env, key+"="+value)
+	}
+
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	command.Run()
+
+	return command.ProcessState.ExitCode()
+}
+
+func getNewEnv(env Environment) map[string]string {
+	envs := os.Environ()
+
+	result := make(map[string]string, len(envs)+len(env))
+
+	for _, envVar := range envs {
+		envPair := strings.SplitN(envVar, "=", 2)
+		result[envPair[0]] = envPair[1]
+	}
+
+	for key, value := range env {
+		_, ok := result[key]
+		if !ok {
+			if !value.NeedRemove {
+				result[key] = value.Value
+			}
+		} else {
+			if value.NeedRemove {
+				delete(result, key)
+			} else {
+				result[key] = value.Value
+			}
+		}
+	}
+
+	return result
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	args := os.Args[1:]
+
+	if len(args) < 2 {
+		fmt.Println("Invalid count of arguments")
+		return
+	}
+
+	env, err := ReadDir(args[0])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	RunCmd(os.Args[2:], env)
 }
